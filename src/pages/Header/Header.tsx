cf4b5783f61fb3e233722339732727ccbe4625dd
bqznya <112616492+bqznya@users.noreply.github.com>
2025-02-26 18:01:10 +0500

    Update Header.tsx

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const Header: React.FC = () => {
     return (
         <header className="header">
-			<nav className="wtf">
-					<Link to="/map" className="head-link">Карта</Link>
-                    <Link to="/" className="head-link">Список персонажей</Link>
-                    <Link to="/create" className="head-link">Создать персонажа</Link>
-                </nav>
+		<nav className="wtf">
+		    <Link to="/map" className="head-link">Карта</Link>
+		    <Link to="/" className="head-link">Список персонажей</Link>
+		    <Link to="/create" className="head-link">Создать персонажа</Link>
+		</nav>
         </header>
 		
     );
